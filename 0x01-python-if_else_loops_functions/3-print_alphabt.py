5cc0af601df6bae892b76873944d9d6dfe2caed7
ishmael1988 <ishmaelsesay155@gmail.com>
2024-01-07 06:40:18 -0800

    modefy

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,7 @@
-for i in range(97, 123):
-    if i != 101 and i != 113:
-        print("{:c}".format(i), end="")
+#!/usr/bin/python3
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) != 'e' and chr(i) != 'q':
+        print('{:c}'.format(i), end='')
+
+
 
