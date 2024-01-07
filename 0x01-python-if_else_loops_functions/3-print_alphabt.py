d18047c501da7fa5d93184ce4c29571eccc76a5d
ishmael1988 <ishmaelsesay155@gmail.com>
2024-01-07 06:38:14 -0800

    modefy

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,4 @@
-# Loop from 97 to 122, which are the ASCII codes for lowercase letters
 for i in range(97, 123):
-    # Use chr() function to convert ASCII code to character
-    # Use end="" parameter to avoid printing a new line
-    # Use if statement to skip q and e
     if i != 101 and i != 113:
-        print(chr(i), end="")
+        print("{:c}".format(i), end="")
 
