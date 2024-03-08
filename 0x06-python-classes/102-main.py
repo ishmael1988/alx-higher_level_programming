ddac64ec5ff1d0ca6383067a6262dd5f81868c5f
ishmael1988 <ishmaelsesay155@gmail.com>
2024-03-08 09:30:49 -0800

    Classes and Objects

---
+++ 0x06-python-classes/102-main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+Square = __import__('102-square').Square
+
+s_5 = Square(5)
+s_6 = Square(6)
+
+if s_5 < s_6:
+    print("Square 5 < Square 6")
+if s_5 <= s_6:
+    print("Square 5 <= Square 6")
+if s_5 == s_6:
+    print("Square 5 == Square 6")
+if s_5 != s_6:
+    print("Square 5 != Square 6")
+if s_5 > s_6:
+    print("Square 5 > Square 6")
+if s_5 >= s_6:
+    print("Square 5 >= Square 6")
+
+guillaume@ubuntu:~/0x06$ ./102-main.py
+Square 5 < Square 6
+Square 5 <= Square 6
+Square 5 != Square 6
